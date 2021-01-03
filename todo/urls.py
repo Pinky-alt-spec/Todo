7c0bb0d734bf4d9a592a72a0477a0d14f3b44e60
Pinky <janeletageng28@gmail.com>
2021-01-03 18:35:40 +0200

    urls back to original

--- todo/urls.py
+++ todo/urls.py
@@ -20,13 +20,14 @@ from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.conf.urls import url
-import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_list.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+# STATIC_URL = '/static/'
+#
+# STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
