ec316be5b04b9b5add18990a83d5606164a80e01
Pinky <janeletageng28@gmail.com>
2021-01-03 18:29:42 +0200

    add os

--- todo/urls.py
+++ todo/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.conf.urls import url
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
