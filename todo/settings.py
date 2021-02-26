342a5b18f2008fcb1271424036593d0bfa3574e9
Pinky Mononyane <64901994+Pinky-alt-spec@users.noreply.github.com>
2021-02-26 06:00:21 -0800

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'j4+o)sb+-r&rm7+a1@cg)nc(nzvhv%4uiiw$h#pm65v&4o58wl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pinks-todos.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pinks-todos.herokuapp.com', 'www.everydaytodo.ml', 'everydaytodo.ml']
 
 
 # Application definition
